be0fb001028f123ce1ab3bae1cce27712fddd773
Kpavicic00 <kpavicic00@fesb.hr>
2021-07-13 17:12:44 +0200

    ch

--- config/prod.py
+++ config/prod.py
@@ -1,7 +1,7 @@
-# import os
+import os
 
-# DEBUG = False
-# SECRET_KEY = 'topsecret'
-# SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
-# SQLALCHEMY_TRACK_MODIFICATIONS = False
+DEBUG = False
+SECRET_KEY = 'topsecret'
+SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+SQLALCHEMY_TRACK_MODIFICATIONS = False
 
